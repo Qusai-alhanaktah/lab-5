b2faeb67fc97db387cbbed98cf764ef348137a34
qusai-alhanaktah <qusaialhanaktah@gmail.com>
2019-12-19 19:45:17 +0200

    check for heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const server = express();
 const PORT = process.env.port || 5501;
+server.use(express.static('public/index.html'))
 
 server.get('/test', (request,response) =>{
     response.send("working");
